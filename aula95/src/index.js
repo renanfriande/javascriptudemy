8717e3a1765288c1b239d57cb9b3461a63ff8806
Renan Friande <renanfriandee@gmail.com>
2025-04-26 18:52:23 -0300

    estudando js 26/04/2025

---
+++ aula95/src/index.js
@@ -0,0 +1,10 @@
+// import {nome, sobrenome, idade, Pessoa as OutraCoisa} from "./modulo1.js"
+// import * as MeuModulo from "./modulo1.js"
+import qualquerNome from "./modulo1.js" // -> qualquerNome importa DEFAULT
+console.log(qualquerNome(5, 5))
+
+
+// const nome = "Pedro"
+// const pessoa = new OutraCoisa("Pedro", "Almirante")
+// console.log(nome, sobrenome, idade)
+// console.log(pessoa)
---
+++ aula95/src/modulo1.js
@@ -0,0 +1,15 @@
+export const nome = "Renan"
+export const sobrenome = "Friande"
+export const idade = 18
+const cpf = 2132131
+
+export default function soma(x, y) {
+    return x+y
+}
+
+export class Pessoa {
+    constructor(nome, sobrenome) {
+        this.nome = nome
+        this.sobrenome = sobrenome
+    }
+}
