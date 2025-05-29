f2d31b9b8e809189fc419a9c97406fd9cf566178
Renan Friande <renanfriandee@gmail.com>
2025-05-29 18:11:21 -0300

    CRUD de alunos

--- api_rest/src/models/Aluno.js
+++ api_rest/src/models/Aluno.js
@@ -4,12 +4,65 @@ import Sequelize, { Model } from 'sequelize';
 export default class Aluno extends Model {
   static init(sequelize) {
     super.init({
-      nome: Sequelize.STRING,
-      sobrenome: Sequelize.STRING,
-      email: Sequelize.STRING,
-      idade: Sequelize.INTEGER,
-      peso: Sequelize.FLOAT,
-      altura: Sequelize.FLOAT,
+      nome: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validade: {
+          len: {
+            args: [3, 255],
+            msg: 'Nome precisa ter entre 3 e 255 caracteres.',
+          },
+        },
+      },
+      sobrenome: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validade: {
+          len: {
+            args: [3, 255],
+            msg: 'Sobrenome precisa ter entre 3 e 255 caracteres.',
+          },
+        },
+      },
+      email: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        unique: {
+          msg: 'Email ja existe.',
+        },
+        validade: {
+          isEmail: {
+            msg: 'Email invalido.',
+          },
+        },
+      },
+      idade: {
+        type: Sequelize.INTEGER,
+        defaultValue: '',
+        validade: {
+          isInt: {
+            msg: 'Idade precisa ser um numero inteiro.',
+          },
+        },
+      },
+      peso: {
+        type: Sequelize.FLOAT,
+        defaultValue: '',
+        validade: {
+          isFloat: {
+            msg: 'Peso precisa ser um numero inteiro ou de ponto flutuante',
+          },
+        },
+      },
+      altura: {
+        type: Sequelize.FLOAT,
+        defaultValue: '',
+        validade: {
+          isFloat: {
+            msg: 'Altura precisa ser um numero inteiro ou de ponto flutuante',
+          },
+        },
+      },
     }, {
       sequelize,
     });
