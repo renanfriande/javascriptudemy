409c0761e11f49617960522a5fb5ebd16dd95c69
Renan Friande <renanfriandee@gmail.com>
2025-05-29 17:45:12 -0300

    aluno routes e email unique migration

--- api_rest/src/controllers/HomeController.js
+++ api_rest/src/controllers/HomeController.js
@@ -1,21 +1,6 @@
-import Aluno from '../models/Aluno';
-
 class HomeController {
   async index(req, res) {
-    try {
-      const novoAluno = await Aluno.create({
-        nome: 'Maria',
-        sobrenome: 'Friande',
-        email: 'maria@gmail.com',
-        idade: 23,
-        peso: 57,
-        altura: 1.6,
-      });
-      res.json(novoAluno);
-    } catch (e) {
-      console.error('ERRO AO CRIAR ALUNO:', e);
-      res.status(400).json({ error: e.message });
-    }
+    res.json('index');
   }
 }
 
---
+++ api_rest/src/database/migrations/20250529203819-mudar-email-aluno-unique.js
@@ -0,0 +1,5 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.changeColumn('alunos', 'email', { type: Sequelize.STRING, allowNull: false, unique: true }),
+
+  down: () => {},
+};
