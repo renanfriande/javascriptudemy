b6facfea2c718967ad5b39bb39e74be74610761d
Renan Friande <renanfriandee@gmail.com>
2025-06-02 17:01:41 -0300

    upload de imagens

---
+++ api_rest/src/routes/fotoRoutes.js
@@ -0,0 +1,14 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Router } from 'express';
+import multer from 'multer';
+
+import FotoController from '../controllers/FotoController';
+import multerConfig from '../config/multerConfig';
+
+const upload = multer(multerConfig);
+
+const router = new Router();
+
+router.post('/', upload.single('foto'), FotoController.store);
+
+export default router;
