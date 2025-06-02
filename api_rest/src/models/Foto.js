6d2e6140c572ea7af889aedac37a065d8cba55e7
Renan Friande <renanfriandee@gmail.com>
2025-06-02 18:32:41 -0300

    relacionando foto-aluno

---
+++ api_rest/src/models/Foto.js
@@ -0,0 +1,35 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import Sequelize, { Model } from 'sequelize';
+
+export default class Foto extends Model {
+  static init(sequelize) {
+    super.init({
+      originalname: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validade: {
+          notEmpty: {
+            msg: 'Campo nao pode ficar vazio.',
+          },
+        },
+      },
+      filename: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validade: {
+          notEmpty: {
+            msg: 'Campo nao pode ficar vazio.',
+          },
+        },
+      },
+    }, {
+      sequelize,
+      tableName: 'fotos',
+    });
+    return this;
+  }
+
+  static associate(models) {
+    this.belongsTo(models.Aluno, { foreignKey: 'aluno_id' });
+  }
+}
