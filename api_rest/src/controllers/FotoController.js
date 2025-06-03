554e2e34c56dfa78acc86c488355a43b152fe579
Renan Friande <renanfriandee@gmail.com>
2025-06-03 17:40:41 -0300

    listando fotos dos alunos

--- api_rest/src/controllers/FotoController.js
+++ api_rest/src/controllers/FotoController.js
@@ -2,6 +2,7 @@
 import multer from 'multer';
 import multerConfig from '../config/multerConfig';
 
+import Aluno from '../models/Aluno';
 import Foto from '../models/Foto';
 
 const upload = multer(multerConfig).single('foto');
@@ -15,11 +16,26 @@ class FotoController {
         });
       }
 
-      const { originalname, filename } = req.file;
-      const { aluno_id } = req.body;
-      const foto = await Foto.create({ originalname, filename, aluno_id });
+      try {
+        const { originalname, filename } = req.file;
+        const { aluno_id } = req.body;
 
-      return res.json(foto);
+        const aluno = await Aluno.findByPk(aluno_id);
+
+        if (!aluno) {
+          return res.status(400).json({
+            errors: ['Aluno nao existe.'],
+          });
+        }
+
+        const foto = await Foto.create({ originalname, filename, aluno_id });
+
+        return res.json(foto);
+      } catch (e) {
+        return res.status(400).json({
+          errors: ['Ocorreu um erro ao salvar a foto.'],
+        });
+      }
     });
   }
 }
--- api_rest/src/routes/userRoutes.js
+++ api_rest/src/routes/userRoutes.js
@@ -10,7 +10,7 @@ const router = new Router();
 // router.get('/', loginRequired, userController.index); // Lista usuarios
 // router.get('/:id', userController.show); // Lista usuario
 
-router.post('/', userController.store);
+router.post('/', loginRequired, userController.store);
 router.put('/', loginRequired, userController.update);
 router.delete('/', loginRequired, userController.delete);
 
