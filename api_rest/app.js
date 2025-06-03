a4500043c60ce6e399c5545f9a018e1cff0d8837
Renan Friande <renanfriandee@gmail.com>
2025-06-03 17:49:20 -0300

    correcao arquivos estaticos

--- api_rest/app.js
+++ api_rest/app.js
@@ -1,5 +1,6 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import dotenv from 'dotenv';
+import { resolve } from 'path';
 
 dotenv.config();
 
@@ -23,6 +24,7 @@ class App {
   middlewares() {
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
+    this.app.use(express.static(resolve(__dirname, 'uploads')));
   }
 
   routes() {
--- api_rest/src/models/Foto.js
+++ api_rest/src/models/Foto.js
@@ -1,5 +1,6 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import Sequelize, { Model } from 'sequelize';
+import appConfig from '../config/appConfig';
 
 export default class Foto extends Model {
   static init(sequelize) {
@@ -20,6 +21,12 @@ export default class Foto extends Model {
           notEmpty: {
             msg: 'Campo nao pode ficar vazio.',
           },
+        },
+      },
+      url: {
+        type: Sequelize.VIRTUAL,
+        get() {
+          return `${appConfig.url}/images/${this.getDataValue('filename')}`;
         },
       },
     }, {
